819b24dee45196f7f9786d898822c5f36efbc7bb
Alan Ramos López <118926925+ARandomGuyZero@users.noreply.github.com>
2024-09-04 14:42:57 -0700

    Create question_model.py
    added question model

---
+++ question_model.py
@@ -0,0 +1,8 @@
+class Question:
+    """
+    Question class that stores a question's text and answer
+    """
+    def __init__(self, text, answer):
+        self.text = text
+        self.answer = answer
+      
